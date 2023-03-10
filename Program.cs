5b08b004915e7dfec1dc41252521551f8b3cf4ce
chrisvj1192 <chrisvega@live.com.mx>
2023-03-10 10:46:17 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivSumasHilos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma s = new Suma();
+
+            DateTime inicio = DateTime.Now;
+
+            //s.SumasSecuenciales();
+            s.SumasConcurrentes();
+            while (s.terminado < 2) { }
+                
+           
+
+            DateTime fin = DateTime.Now;
+
+            TimeSpan diff = fin -inicio;
+
+            Console.WriteLine(diff.TotalMilliseconds);
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Suma.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ActivSumasHilos
+{
+    internal class Suma
+    {
+        public int terminado = 0;
+        public void SumasSecuenciales()
+        {
+            int res;
+            Random r = new Random();
+            for(int i=0; i< int.MaxValue;i++)
+            {
+                res = r.Next() + r.Next();
+            }
+        }
+
+        public void SumasConcurrentes()
+        {
+            Thread t1 = new Thread(HiloSumas);
+            Thread t2 = new Thread(HiloSumas);
+
+            t1.Start();
+            t2.Start();
+        }
+
+        public void HiloSumas()
+        {
+            int res;
+            Random r = new Random();
+
+            int total = int.MaxValue / 2;
+
+            for (int i = 0; i < total ; i++)
+                res = r.Next() + r.Next();
+
+            terminado++;
+        }
+    }
+}
